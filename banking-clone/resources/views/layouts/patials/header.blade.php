9ea467b72d3e3cc65db0a82bba066d7f9aebda89
“techdeft” <wearetechdeft@gmail.com>
2023-03-07 00:34:54 +0100

    Side Nav and logout function

--- banking-clone/resources/views/layouts/patials/header.blade.php
+++ banking-clone/resources/views/layouts/patials/header.blade.php
@@ -76,96 +76,7 @@
                                         </div>
                                     </div>
                                 </li>
-                                <li class="list-group-item">
-                                    <div class="row">
-                                        <div class="col">
-                                            <a class="text-body" href="#">
-                                                <div class="d-flex">
-                                                    <img src="{{ asset('assets/images/avatar/avatar-2.jpg') }}"
-                                                        alt="" class="avatar-md rounded-circle">
-                                                    <div class="ms-3">
-                                                        <h5 class="fw-bold mb-1">Brooklyn Simmons</h5>
-                                                        <p class="mb-3">
-                                                            Just launched a new Courses React for Beginner.
-                                                        </p>
-                                                        <span class="fs-6 text-muted">
-                                                            <span><span
-                                                                    class="fe fe-thumbs-up text-success me-1"></span>Oct
-                                                                9,</span>
-                                                            <span class="ms-1">1:20 PM</span>
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="col-auto text-center me-2">
-                                            <a href="#" class="badge-dot bg-secondary" data-bs-toggle="tooltip"
-                                                data-bs-placement="top" title="Mark as unread">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="list-group-item">
-                                    <div class="row">
-                                        <div class="col">
-                                            <a class="text-body" href="#">
-                                                <div class="d-flex">
-                                                    <img src="{{ asset('assets/images/avatar/avatar-3.jpg') }}"
-                                                        alt="" class="avatar-md rounded-circle">
-                                                    <div class="ms-3">
-                                                        <h5 class="fw-bold mb-1">Jenny Wilson</h5>
-                                                        <p class="mb-3">
-                                                            Krisitn Watsan like your comment on course
-                                                            Javascript Introduction!
-                                                        </p>
-                                                        <span class="fs-6 text-muted">
-                                                            <span><span
-                                                                    class="fe fe-thumbs-up text-info me-1"></span>Oct
-                                                                9,</span>
-                                                            <span class="ms-1">1:56 PM</span>
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="col-auto text-center me-2">
-                                            <a href="#" class="badge-dot bg-secondary" data-bs-toggle="tooltip"
-                                                data-bs-placement="top" title="Mark as unread">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="list-group-item">
-                                    <div class="row">
-                                        <div class="col">
-                                            <a class="text-body" href="#">
-                                                <div class="d-flex">
-                                                    <img src="{{ asset('assets/images/avatar/avatar-4.jpg') }}"
-                                                        alt="" class="avatar-md rounded-circle">
-                                                    <div class="ms-3">
-                                                        <h5 class="fw-bold mb-1">Sina Ray</h5>
-                                                        <p class="mb-3">
-                                                            You earn new certificate for complete the
-                                                            Javascript
-                                                            Beginner course.
-                                                        </p>
-                                                        <span class="fs-6 text-muted">
-                                                            <span><span
-                                                                    class="fe fe-award text-warning me-1"></span>Oct
-                                                                9,</span>
-                                                            <span class="ms-1">1:56 PM</span>
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </div>
-                                        <div class="col-auto text-center me-2">
-                                            <a href="#" class="badge-dot bg-secondary" data-bs-toggle="tooltip"
-                                                data-bs-placement="top" title="Mark as unread">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
+
                             </ul>
                             <div class="border-top px-3 pt-3 pb-0">
                                 <a href="../notification-history.html" class="text-link fw-semi-bold">
@@ -177,8 +88,8 @@
                 </li>
                 <!-- List -->
                 <li class="dropdown ms-2">
-                    <a class="rounded-circle" href="#" role="button" id="dropdownUser"
-                        data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="rounded-circle" href="#" role="button" id="dropdownUser" data-bs-toggle="dropdown"
+                        aria-expanded="false">
                         <div class="avatar avatar-md avatar-indicators avatar-online">
                             <img alt="avatar" src="{{ asset('assets/images/avatar/avatar-1.jpg') }}"
                                 class="rounded-circle">
@@ -192,50 +103,20 @@
                                         class="rounded-circle">
                                 </div>
                                 <div class="ms-3 lh-1">
-                                    <h5 class="mb-1">Annette Black</h5>
-                                    <p class="mb-0 text-muted">annette@geeksui.com</p>
+                                    <h5 class="mb-1">{{ Auth::user()->first_name }}</h5>
+                                    <p class="mb-0 text-muted">{{ Auth::user()->email }}</p>
                                 </div>
                             </div>
                         </div>
                         <div class="dropdown-divider"></div>
                         <ul class="list-unstyled">
-                            <li class="dropdown-submenu dropstart-lg">
-                                <a class="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">
-                                    <i class="fe fe-circle me-2"></i> Status
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a class="dropdown-item" href="#">
-                                            <span class="badge-dot bg-success me-2"></span> Online
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="#">
-                                            <span class="badge-dot bg-secondary me-2"></span> Offline
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="#">
-                                            <span class="badge-dot bg-warning me-2"></span> Away
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="#">
-                                            <span class="badge-dot bg-danger me-2"></span> Busy
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
+
                             <li>
                                 <a class="dropdown-item" href="../profile-edit.html">
                                     <i class="fe fe-user me-2"></i> Profile
                                 </a>
                             </li>
-                            <li>
-                                <a class="dropdown-item" href="../student-subscriptions.html">
-                                    <i class="fe fe-star me-2"></i> Subscription
-                                </a>
-                            </li>
+
                             <li>
                                 <a class="dropdown-item" href="#">
                                     <i class="fe fe-settings me-2"></i> Settings
@@ -245,9 +126,16 @@
                         <div class="dropdown-divider"></div>
                         <ul class="list-unstyled">
                             <li>
-                                <a class="dropdown-item" href="/">
-                                    <i class="fe fe-power me-2"></i> Sign Out
-                                </a>
+                                <form action="{{ route('logout') }}" method="post">
+                                    @csrf
+
+                                    <a class="dropdown-item btn btn"
+                                        onclick="event.preventDefault();
+                                    this.closest('form').submit();">
+                                        <i class="fe fe-power me-2"></i> Sign Out
+                                    </a>
+                                </form>
+
                             </li>
                         </ul>
                     </div>
--- banking-clone/resources/views/layouts/patials/side-nav.blade.php
+++ banking-clone/resources/views/layouts/patials/side-nav.blade.php
@@ -8,181 +8,18 @@
         <!-- Navbar nav -->
         <ul class="navbar-nav flex-column" id="sideNavbar">
             <li class="nav-item">
-                <a class="nav-link  " href="#" data-bs-toggle="collapse" data-bs-target="#navDashboard"
-                    aria-expanded="false" aria-controls="navDashboard">
+                <a class="nav-link  " href="{{ route('dashboard') }}">
                     <i class="nav-icon fe fe-home me-2"></i> Dashboard
                 </a>
-                <div id="navDashboard" class="collapse  show " data-bs-parent="#sideNavbar">
-                    <ul class="nav flex-column">
-                        <li class="nav-item ">
-                            <a class="nav-link  active " href="admin-dashboard.html">
-                                Overview
-                            </a>
-                        </li>
-                        <!-- Nav item -->
-                        <li class="nav-item ">
-                            <a class="nav-link " href="dashboard-analytics.html">
-                                Analytics
 
-                            </a>
-                        </li>
-                    </ul>
-                </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link  collapsed " href="#" data-bs-toggle="collapse" data-bs-target="#navCourses"
-                    aria-expanded="false" aria-controls="navCourses">
-                    <i class="nav-icon fe fe-book me-2"></i> Courses
+                <a class="nav-link  " href="{{ route('airtime') }}">
+                    <i class="nav-icon fe fe-phone-forwarded me-2"></i> Buy Airtime
                 </a>
-                <div id="navCourses" class="collapse " data-bs-parent="#sideNavbar">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link " href="admin-course-overview.html">
-                                All Courses
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="admin-course-category.html">
-                                Courses Category
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="admin-course-category-single.html">
-                                Category Single
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <!-- Nav item -->
-            <li class="nav-item">
-                <a class="nav-link   collapsed " href="#" data-bs-toggle="collapse" data-bs-target="#navProfile"
-                    aria-expanded="false" aria-controls="navProfile">
-                    <i class="nav-icon fe fe-user me-2"></i> User
-                </a>
-                <div id="navProfile" class="collapse " data-bs-parent="#sideNavbar">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link " href="admin-instructor.html">
-                                Instructor
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="admin-students.html">Students</a>
-                        </li>
-                    </ul>
-                </div>
+
             </li>
 
-            <!-- Nav item -->
-            <li class="nav-item ">
-                <a class="nav-link   collapsed  " href="#" data-bs-toggle="collapse" data-bs-target="#navCMS"
-                    aria-expanded="false" aria-controls="navCMS">
-                    <i class="nav-icon fe fe-book-open me-2"></i> CMS
-                </a>
-                <div id="navCMS" class="collapse  " data-bs-parent="#sideNavbar">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link   active  " href="admin-cms-overview.html">
-                                Overview
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link  " href="admin-cms-post.html">
-                                All Post
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="admin-cms-post-new.html">
-                                New Post
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="admin-cms-post-category.html">
-                                Category
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <!-- Nav item -->
-            <li class="nav-item ">
-                <a class="nav-link   collapsed  " href="#" data-bs-toggle="collapse" data-bs-target="#navProject"
-                    aria-expanded="false" aria-controls="navProject">
-                    <i class="nav-icon fe fe-file me-2"></i> Project
-                </a>
-                <div id="navProject" class="collapse  " data-bs-parent="#sideNavbar">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link   " href="project-grid.html">
-                                Grid
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link  " href="project-list.html">
-                                List
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link   collapsed  " href="#" data-bs-toggle="collapse"
-                                data-bs-target="#navprojectSingle" aria-expanded="false"
-                                aria-controls="navprojectSingle">
-
-                                Single
-                            </a>
-                            <div id="navprojectSingle" class="collapse " data-bs-parent="#navProject">
-                                <ul class="nav flex-column">
-                                    <li class="nav-item">
-                                        <a class="nav-link  active " href="project-overview.html">
-                                            Overview
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link " href="project-task.html">
-                                            Task
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link " href="project-budget.html">
-                                            Budget
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link " href="project-team.html">
-                                            Team
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link " href="project-files.html">
-                                            Files
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link " href="project-summary.html">
-                                            Summary
-                                        </a>
-                                    </li>
-
-                                </ul>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="add-project.html">
-                                Create Project
-                            </a>
-                        </li>
-
-
-
-
-
-
-
-
-
-                    </ul>
-                </div>
-            </li>
 
 
 
